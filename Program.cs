dc0511bd5195e2cdd42bf05af8e46ef4127e109a
Zachary Townes <zachtownes@gmail.com>
2019-10-08 08:21:50 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No Hello!");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
